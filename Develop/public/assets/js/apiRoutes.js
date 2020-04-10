00a9e3a80d3189858cdb82b7970e12860e344249
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-10 16:05:40 -0700

    Add api and html route modules

---
+++ Develop/public/assets/js/apiRoutes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = (app) => {
+app.get("/api/notes", (req, res) => {
+  const database = path.join(__dirname, "../../../db/db.json");
+  fs.readFile(database, (err, data) => {
+    if(err){
+      console.log(err)
+    };
+    const info = JSON.parse(data)
+    res.json(info)
+  })
+
+});
+}
