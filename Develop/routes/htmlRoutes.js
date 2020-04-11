e0dc04552010ae204f353fcda4c36ca1f17006a2
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-10 17:31:56 -0700

    Serve mulitple files

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const express = require("express");
 
 module.exports = (app) => {
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+  app.use(express.static("public", { extensions: ["html"] }));
+  // app.get("/notes", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/notes.html"));
+  // });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 }
