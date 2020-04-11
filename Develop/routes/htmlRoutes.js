d65861c79fb8522928bcb9a597d6ba089c9d3630
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-10 20:24:17 -0700

    Change default route to homepage if no designated path

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const express = require("express");
 module.exports = (app) => {
   app.use(express.static("./Develop/public", { extensions: ["html"] }));
 
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 }
